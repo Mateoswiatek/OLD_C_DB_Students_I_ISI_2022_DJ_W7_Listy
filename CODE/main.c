f3cdc6f4a9d0fcde10643cdd480547978b120c94
Mateoswiatek <mateoswiatek@student.agh.edu.pl>
2022-12-03 15:39:44 +0100

    wskaznik_do_wskaznika

--- CODE/main.c
+++ CODE/main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define WYJSCIE 9
+struct student *root; //pierwszy element
 /*
     na start programu tworzony jest pierwszy obiekt.
 
-    1.dodawanie studentÛw danπ iloúÊ,
-        1. tworzenie przestrzeni obiektu na koÒcu listy.
-            X 1.przejúcie do koÒca listy (tylko raz)
-            X 2.zapamiÍtanie wskaünika do ostatniego elementu
+    1.dodawanie student√≥w dan¬π ilo≈ì√¶,
+        1. tworzenie przestrzeni obiektu na ko√±cu listy.
+            X 1.przej≈ìcie do ko√±ca listy (tylko raz)
+            X 2.zapami√™tanie wska≈∏nika do ostatniego elementu
         2. wprowadzanie danych
 
-    2.wyúwietlanie wszystkich studentÛw
+    2.wy≈ìwietlanie wszystkich student√≥w
 
-    czyszczenie po kaødym przejsciu, po kazdym wpisanym studencie
+    czyszczenie po ka¬ødym przejsciu, po kazdym wpisanym studencie
 
 */
 struct student{
@@ -23,8 +26,13 @@ struct student{
 void add_students(int ilosc, struct student *ws_listy){ // ile i do ktorej listy
     struct student *current_student, *new_student;
     int i=0;
-    if(!ws_listy){ //jeúli nie ma jeszcze listy
+    if(!ws_listy){ //jesli nie ma jeszcze listy
         ws_listy=malloc(sizeof(struct student));
+        printf("przed zmiana\n");
+        *root=ws_listy; // przypisanie roota do pierwszego elemtnu
+        printf("po zmianie\n");
+        fflush(stdout);
+
         wpisz_dane_studenta(ws_listy);
         ws_listy->next_adress=0;
         i++; // bo juz jednego utworzylismy
@@ -32,14 +40,14 @@ void add_students(int ilosc, struct student *ws_listy){ // ile i do ktorej listy
     current_student=ws_listy;
 
     while(current_student->next_adress){              // Dopoki istnieje kolejny obiekt, przechodzimy do kolejnego obiektu, bo musimy dojsc do ostatniego elmentu
-        current_student=current_student->next_adress; // Przechodzimy do kolejnych, dopÛki jest nastepny obiekt
+        current_student=current_student->next_adress; // Przechodzimy do kolejnych, dop√≥ki jest nastepny obiekt
     }
     // jestesmy na koncu listy, current wskazuje na ostatni obiekt
     for(i; i<ilosc;i++){
 
         current_student->next_adress=malloc(sizeof(struct student)); // (1) w aktualneej strokturze wpisujemy adres do kolejnego
 
-        current_student=current_student->next_adress; // (2) //current zaczyna wskazywaÊ na nowo utworzony
+        current_student=current_student->next_adress; // (2) //current zaczyna wskazywa√¶ na nowo utworzony
 
         if(current_student==NULL){printf("\n=========================\n      blad pamieci\n=========================\n"); return 0;}
 
@@ -47,7 +55,7 @@ void add_students(int ilosc, struct student *ws_listy){ // ile i do ktorej listy
     }
 }
 
-void wpisz_dane_studenta(struct student *s){ // przekazujemy wskaünik na konkretny element
+void wpisz_dane_studenta(struct student *s){ // przekazujemy wska≈∏nik na konkretny element
     printf("podaj ocene: \n");
     scanf("%d", &s->ocena); // tutaj to co wprowadzimy, jest przekazywane do obiektu s
     printf("Podaj nazwisko: \n");
@@ -56,45 +64,71 @@ void wpisz_dane_studenta(struct student *s){ // przekazujemy wskaünik na konkret
 }
 
 void wyswietl( struct student s){ // wypisuje konkretny obiekt strukturalny
-    printf("Student %s ma ocene %d, nastepny wskaznik to %d\n", s.nazwisko, s.ocena, s.next_adress);
+    printf("Student %s ma ocene %d\n", s.nazwisko, s.ocena);
 }
 
-
-
-
-
-
-
-
-
-
 void wyswietl_all(struct student *lista_studentow){
+    system("cls");
     if(!lista_studentow){
-            printf("Nie ma zadnego studenta na liscie dodaj wpisujac 1");
+            printf("Nie ma zadnego studenta na liscie dodaj wybierajac z menu 1\n");
             return 0; // nie ma elementow
+    }
+    else{
+        while(lista_studentow->next_adress){
+            wyswietl(*lista_studentow);
+            lista_studentow=lista_studentow->next_adress;
+        }
+        wyswietl(*lista_studentow);
     }
 }
 
 int main()
 {
-    struct student *root; //pierwszy element
-    root=0;
+    while(1){
+        int wybor, ilosc, pom;
 
-    //tu bÍdzie while(1) a w nim case (menu co chce zrobiÊ)
-    //while(1){
-        int wybor;
-        printf("Wybierz dzialanie:\n0 - dodawanie studentow(nawet jesli nie ma jeszcze listy)\n1 - wyswietlanie wszystkich studentow\n2 - znajdywanie studenta / tow, rozbudowac o wybor np wszyscy co maja dana ocene\nw tym zrobic tez dodatkowe operacje co z tymi zrobic(wyswietlic, usunac) 3 - wyjscie\n");
+        printf("Wybierz dzialanie:\n0 - dodawanie studentow(nawet jesli nie ma jeszcze listy)\n1 - wyswietlanie wszystkich studentow\n");
+        printf("2 - znajdywanie studenta / tow, rozbudowac o wybor np wszyscy co maja dana ocene\nw tym zrobic tez dodatkowe operacje co z tymi zrobic(wyswietlic, usunac)\n %d - wyjscie\n", WYJSCIE);
         scanf("%d", &wybor);
 
-    add_students(3, root); // wpisujemy dane pierwszego studenta
+        system("cls"); // oczyszczanie konsoli
+        switch(wybor){
+        case 0:
+            printf("Podaj liczbe studentow: \n");
+            scanf("%d", &ilosc);
+            system("cls");
+            add_students(ilosc, root); // wpisujemy dane studentow, tyle ile wpisano
 
-    //printf("%d\n", root); // wyúwietlamy aktualnπ wartoúÊ wskaünika na roota
+            printf("root %d\n",root);
+
+            printf("chcesz wyswietlic? 0/1 \n");
+            scanf("%d", &pom);
+            if(pom) wyswietl_all(root);
+            break;
+        case 1:
+            printf("wyswietlanie wszystkich studentow\n");
+            wyswietl_all(root);
+            break;
+
+        case WYJSCIE:
+            free(root);
+            printf("dzieki za skorzystanie z programu\n");
+            fflush(stdout); // z bufora na stdout (standardowe wyj≈õcie); // pokazac ze bez tego nie wypisze // ew mozna zamienic z free(root)
+            return 0;
+
+        }
+
+    //printf("%d\n", root); // wy≈ìwietlamy aktualn¬π warto≈ì√¶ wska≈∏nika na roota
     //printf("Student %s ma ocene %d\n", &root->nazwisko, &root->ocena)
+    if(wybor!=WYJSCIE){
+        printf("wrocic do menu? 0/1\n", &pom);
+        scanf("%d", &pom);
+        if(!pom) break;
 
-    //wyswietl(*root);
-
-    free(root);
-
-    //}
+    }
+        system("cls");
+        free(root);
+    }
+    printf("dzieki za skorzystanie z programu\n");
     return 0;
 }
